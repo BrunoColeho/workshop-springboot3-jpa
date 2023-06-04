2e9dabcea1fce3999c3ecee0628a52f0add9f968
Bruno Coelho <craft.bruno@hotmail.com>
2023-06-04 14:49:54 -0300

    Category

--- src/main/java/com/brunostests/course/config/TestConfig.java
+++ src/main/java/com/brunostests/course/config/TestConfig.java
@@ -11,8 +11,10 @@ import org.springframework.context.annotation.Profile;
 import com.brunostests.course.entities.Order;
 import com.brunostests.course.entities.User;
 import com.brunostests.course.entities.enums.OrderStatus;
+import com.brunostests.course.repositories.CategoryRepository;
 import com.brunostests.course.repositories.OrderRepository;
 import com.brunostests.course.repositories.UserRepository;
+import com.brunostests.course.entities.Category;
 
 @Configuration
 @Profile("test")
@@ -23,11 +25,24 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers"); 
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2); 
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
